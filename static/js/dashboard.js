b8bc351f0c603b2701c1789b9fabf4904929b665
Kairos-T <contact@kairostay.com>
2024-09-29 22:03:43 +0800

    fix: change error message within modal to a toast

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -9,12 +9,13 @@ function congrats() {
 
 function getBalance() {
   var balance = parseInt(getCookies(document.cookie).balance);
-  document.getElementById(
-    "balance"
-  ).innerHTML = `$${balance.toLocaleString("en-US", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })}`;
+  document.getElementById("balance").innerHTML = `$${balance.toLocaleString(
+    "en-US",
+    {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }
+  )}`;
 }
 
 const getCookies = (cookieStr) =>
@@ -29,10 +30,14 @@ const getCookies = (cookieStr) =>
 function displayErrorMessage() {
   const urlParams = new URLSearchParams(window.location.search);
   const errorMessage = urlParams.get("error");
+
   if (errorMessage) {
     const errorElement = document.getElementById("error-message");
     errorElement.textContent = decodeURIComponent(errorMessage);
-    errorElement.style.display = "block";
+    const errorToast = new bootstrap.Toast(
+      document.getElementById("errorToast")
+    );
+    errorToast.show();
   }
 }
 
@@ -42,18 +47,27 @@ function displaySuccessMessage() {
   if (successMessage) {
     const successElement = document.getElementById("success-message");
     successElement.textContent = decodeURIComponent(successMessage);
-    successElement.style.display = "block";
+    const successToast = new bootstrap.Toast(
+      document.getElementById("successToast")
+    );
+    successToast.show();
   }
 }
 
 function displayUsername() {
-  const username = getCookies(document.cookie).login.split(":")[0].replace("\"", "");
+  const username = getCookies(document.cookie)
+    .login.split(":")[0]
+    .replace('"', "");
   const usernameElement = document.getElementById("welcome-message");
   usernameElement.textContent = `Welcome, ${username}!`;
 }
 
 function logout() {
-  document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+  document.cookie.split(";").forEach(function (c) {
+    document.cookie = c
+      .replace(/^ +/, "")
+      .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+  });
   window.location.href = "/";
 }
 
